a7b9ffafd5e8006b84f25812675a7014b70f190e
lsquantum <86186932+lsquantum@users.noreply.github.com>
2021-12-02 12:53:45 +0300

    Exception models added.

---
+++ Appcent.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appcent.Application.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public ValidationException(string message) : base(message) { }
+        public ValidationException(string message, Exception innerException) : base(message, innerException) { }
+        public ValidationException() : base("One or more validation failures have occurred.")
+        {
+            Errors = new List<string>();
+        }
+        public List<string> Errors { get; }
+        public ValidationException(IEnumerable<ValidationFailure> failures) : this()
+        {
+            foreach (var failure in failures)
+            {
+                Errors.Add(failure.ErrorMessage);
+            }
+        }
+    }
+}
