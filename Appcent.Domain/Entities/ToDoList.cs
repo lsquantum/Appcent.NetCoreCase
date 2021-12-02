4b515cdc80e57e19416a89a66743c66988bc9b93
lsquantum <86186932+lsquantum@users.noreply.github.com>
2021-12-02 10:18:53 +0300

    Entity added.

--- Appcent.Domain/Entities/ToDoList.cs
+++ Appcent.Domain/Entities/ToDoList.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 
 namespace Appcent.Domain.Entities
 {
+    public enum TaskStatus { InProgress, Done, Postponed, Cancel }
     public class ToDoList
     {
         public string TaskName { get; set; }
+        public TaskStatus TaskStatus { get; set; } = TaskStatus.InProgress;
         public DateTime Created { get; set; } = DateTime.Now;
-        public string User { get; set; }
-        public string UserName { get; set; }
-        public string Password { get; set; }
+        public string UserId { get; set; }
         public string Type => "ToDoList";
     }
 }
