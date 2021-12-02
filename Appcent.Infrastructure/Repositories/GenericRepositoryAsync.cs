1e34d4629d7a8d08eb0bef530c72ae692bbaabaf
lsquantum <86186932+lsquantum@users.noreply.github.com>
2021-12-02 10:27:25 +0300

    GenericRepo methods added.

--- Appcent.Infrastructure/Repositories/GenericRepositoryAsync.cs
+++ Appcent.Infrastructure/Repositories/GenericRepositoryAsync.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Appcent.Application.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,34 @@ using System.Threading.Tasks;
 
 namespace Appcent.Infrastructure.Repositories
 {
-    class GenericRepositoryAsync
+    public class GenericRepositoryAsync<T> : IGenericRepositoryAsync<T> where T : class
     {
+        public GenericRepositoryAsync()
+        {
+
+        }
+        public virtual async Task<T> GetByIdAsync(int id)
+        {
+            return null;
+        }
+        public async Task<IList<T>> GetAllAsync()
+        {
+            return null;
+
+        }
+        public async Task<T> AddAsync(T entity)
+        {
+            return null;
+
+        }
+
+        public virtual async Task UpdateAsync(T entity)
+        {
+            return;
+        }
+        public virtual async Task DeleteAsync(T entity)
+        {
+            return;
+        }
     }
 }
