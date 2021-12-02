58d3a15850c3b0d160fb19c095875351bafe3b09
lsquantum <86186932+lsquantum@users.noreply.github.com>
2021-12-02 10:35:36 +0300

    Couchbase config added.

--- Appcent.Infrastructure/ServiceRegistration.cs
+++ Appcent.Infrastructure/ServiceRegistration.cs
@@ -1,5 +1,6 @@
 ﻿using Appcent.Application.Interfaces;
 using Appcent.Infrastructure.Repositories;
+using Couchbase.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -14,6 +15,10 @@ namespace Appcent.Infrastructure
     {
         public static void AddInfrastructure(this IServiceCollection services, IConfiguration _config)
         {
+            //Add Couchbase
+            services.AddCouchbase(_config.GetSection("Couchbase"));
+            services.AddCouchbaseBucket<INamedBucketProvider>("default");
+
             #region Repositories
 
             services.AddTransient(typeof(IGenericRepositoryAsync<>), typeof(GenericRepositoryAsync<>));
