4134c20035a7750ebe19fbdb3bf990c2f5f5bfa4
lsquantum <86186932+lsquantum@users.noreply.github.com>
2021-12-02 10:23:25 +0300

    Repository, Interface added.

---
+++ Appcent.Application/Interfaces/IGenericRepositoryAsync.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appcent.Application.Interfaces
+{
+    public interface IGenericRepositoryAsync<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IList<T>> GetAllAsync();
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
