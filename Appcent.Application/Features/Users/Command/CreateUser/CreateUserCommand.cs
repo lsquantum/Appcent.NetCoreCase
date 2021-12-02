118bbd289c8c5fca00316ec76df7210279bc43d5
lsquantum <86186932+lsquantum@users.noreply.github.com>
2021-12-02 15:28:45 +0300

    User methods added.

---
+++ Appcent.Application/Features/Users/Command/CreateUser/CreateUserCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appcent.Application.Features.Users.Command.CreateUser
+{
+    class CreateUserCommand
+    {
+    }
+}
