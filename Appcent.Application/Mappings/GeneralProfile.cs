9fa42e41a38b1dd124364ced0b25b61bb19eb736
lsquantum <86186932+lsquantum@users.noreply.github.com>
2021-12-02 12:57:35 +0300

    AutoMapper, FluentValidation added.

---
+++ Appcent.Application/Mappings/GeneralProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appcent.Application.Mappings
+{
+    public class GeneralProfile : Profile
+    {
+        public GeneralProfile()
+        {
+        }
+    }
+}
