5658a7e77a203a98196b618a43abe7e587c9a239
lsquantum <86186932+lsquantum@users.noreply.github.com>
2021-12-02 13:17:16 +0300

    DI added for app layer

---
+++ Appcent.Application/ServiceExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using FluentValidation;
+using MediatR;
+using Appcent.Application.Behaviours;
+
+namespace Appcent.Application
+{
+    public static class ServiceExtensions
+    {
+        public static void AddApplicationLayer(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+        }
+    }
+}
